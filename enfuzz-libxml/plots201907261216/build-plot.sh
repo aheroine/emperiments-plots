a7b1a39f24cdd7ea750243c3776827a81d5e1e7f
jenkins <jenkins@360.cn11>
2019-07-25 21:19:12 -0700

    libxml

---
+++ enfuzz-libxml/plots201907261216/build-plot.sh
@@ -0,0 +1,16 @@
+#./build-plot.sh  ~/projects/libxml ~/projects/libxml/plots
+#~/afl/afl-plot ../out-enfuzz-RQ/monitor ./out-enfuzz-RQ/monitori
+plot=~/afl/afl-plot
+project=$1
+out=$2
+for i in "out-enfuzz-RQ" "out-enfuzz-Q" "out-enfuzz-A"
+do
+	echo $i
+	for j in "afl" "aflfast" "fairifuzz" "monitor"
+	do
+		#echo $project/$i/$j
+		$plot $project/$i/$j $out/$i/$j
+		#echo $project/$i/$j
+	done
+done
+
